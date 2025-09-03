21d886d8abbdab960356e299a90ed3c75abdfa1b
AmaliJD <amali.jdelauney@gmail.com>
2025-09-03 01:06:00 -0400

    fix overlay text alignment

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -7,7 +7,7 @@ export function formatDate(date: Date): string
 
     const year = date.getFullYear();
     const month = (date.getMonth() + 1).toString().padStart(2, "0"); // months are 0-indexed
-    const day = date.getDate().toString().padStart(2, "0");
+    const day = (date.getDate() + 1).toString().padStart(2, "0");
 
     const separator = " / ";
 
